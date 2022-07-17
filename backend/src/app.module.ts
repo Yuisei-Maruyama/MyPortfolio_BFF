59cca04645710e568f9bd2378e43c5b218518df5
Yuisei-Maruyama <yuisei.metro@gmail.com>
2022-07-18 00:29:20 +0900

    update: Module設定

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CommonModule } from './common/common.module'
+import { TestModule } from './test/test.module';
 
 @Module({
-  imports: [],
+  imports: [
+    CommonModule,
+    TestModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
