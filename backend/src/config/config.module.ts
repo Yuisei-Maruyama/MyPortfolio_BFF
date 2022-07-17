8e79e3b19df4002b4bdf56f9bf7e64ced985784d
Yuisei-Maruyama <yuisei.metro@gmail.com>
2022-07-18 00:30:44 +0900

    add: configモジュールの作成

---
+++ backend/src/config/config.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule as NestConfigModule } from '@nestjs/config'
+
+@Module({
+  imports: [
+    NestConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env.development.local'],
+      // ignoreEnvFile: true,
+    }),
+  ],
+})
+export class ConfigModule {}
